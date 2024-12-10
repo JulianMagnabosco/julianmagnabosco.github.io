bcf8c67511178f611c1578441159296bd95428e0
Raishin0 <julian.magnabosco@gmail.com>
2024-12-10 18:11:49 -0300

    ya va teniendo forma

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TextGeometry } from "three/src/geometries/TextGeometry.js";
-import { FontLoader } from 'three/src/loaders/FontLoader.js';
+import { FontLoader } from "three/src/loaders/FontLoader.js";
 
 // Setup
 
@@ -26,13 +26,10 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-// Torus
+// Background
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-// scene.add(torus);
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
 
 // Lights
 
@@ -50,61 +47,31 @@ scene.add(pointLight, ambientLight);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 
+// Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+
+// scene.add(torus);
+
+//Stars
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+  let [x, y, z] = [0,0,0];
+
+  x=THREE.MathUtils.randFloatSpread(100)
+  y=THREE.MathUtils.randFloatSpread(200)
+  z=THREE.MathUtils.randFloatSpread(100)
 
   star.position.set(x, y, z);
   scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
-
-// Background
-
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
-scene.background = spaceTexture;
-
-//Text
-const loader = new FontLoader();
-// promisify font loading
-function loadFont(url) {
-  return new Promise((resolve, reject) => {
-    loader.load(url, resolve, undefined, reject);
-  });
-}
-const font = await loadFont(
-  "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json"
-);
-
-const geometryText = new TextGeometry("Hello three.js!", {
-  font: font,
-  size: 80,
-  depth: 5,
-  curveSegments: 12,
-  bevelEnabled: true,
-  bevelThickness: 10,
-  bevelSize: 8,
-  bevelOffset: 0,
-  bevelSegments: 5,
-});
-
-const materialText = new THREE.MeshFaceMaterial([
-  new THREE.MeshPhongMaterial({
-    color: 0xff22cc,
-    flatShading: true,
-  }), // front
-  new THREE.MeshPhongMaterial({
-    color: 0xffcc22,
-  }), // side
-]);
-const text = new THREE.Mesh(geometryText, materialText);
-scene.add(text);
 
 // Avatar
 
@@ -130,15 +97,70 @@ const moon = new THREE.Mesh(
   })
 );
 
-scene.add(moon);
+// scene.add(moon);
 
-moon.position.z = 0;
+//Text
+
+const loader = new THREE.FontLoader();
+
+loader.load("Unitblock_Regular.json", function (font) {
+  const geometryText = new THREE.TextGeometry("three.js", {
+    font: font,
+    size: 5,
+    height: 1,
+    curveSegments: 10,
+    bevelEnabled: false,
+    bevelOffset: 0,
+    bevelSegments: 1,
+    bevelSize: 0.3,
+    bevelThickness: 1,
+  });
+  geometryText.center()
+  const materialsText = [
+    new THREE.MeshPhongMaterial({ color: 0xffffff }), // front
+    new THREE.MeshPhongMaterial({ color: 0x000000 }), // side
+  ];
+  const textMesh1 = new THREE.Mesh(geometryText, materialsText);
+  textMesh1.position.x = 0;
+
+  const boundingBox = new THREE.Box3().setFromObject(textMesh1);
+  const size = boundingBox.getSize();
+  console.log(size);
+
+  scene.add(textMesh1);
+});
+
+//Forma
+const heartShape = new THREE.Shape();
+
+heartShape.moveTo(25, 25);
+heartShape.bezierCurveTo(25, 25, 20, 0, 0, 0);
+heartShape.bezierCurveTo(-30, 0, -30, 35, -30, 35);
+heartShape.bezierCurveTo(-30, 55, -10, 77, 25, 95);
+heartShape.bezierCurveTo(60, 77, 80, 55, 80, 35);
+heartShape.bezierCurveTo(80, 35, 80, 0, 50, 0);
+heartShape.bezierCurveTo(35, 0, 25, 25, 25, 25);
+
+const extrudeSettings = {
+  depth: 8,
+  bevelEnabled: true,
+  bevelSegments: 2,
+  steps: 2,
+  bevelSize: 1,
+  bevelThickness: 1,
+};
+
+const geometryShape = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
+const meshShape = new THREE.Mesh(geometryShape, new THREE.MeshPhongMaterial());
+// scene.add(meshShape);
+
+//Positios
+
+moon.position.y = 0;
 moon.position.x = 0;
-text.position.z = 30;
-text.position.x = -10;
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+jeff.position.y = -10;
+jeff.position.x = 0;
 
 // Scroll Animation
 
@@ -151,9 +173,7 @@ function moveCamera() {
   jeff.rotation.y += 0.01;
   jeff.rotation.z += 0.01;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+  camera.position.y = t * 0.01;
 }
 //Rezize
 
