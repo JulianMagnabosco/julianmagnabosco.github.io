96fb6e736104541fce226d9a4f4ad83169efeb28
Raishin0 <julian.magnabosco@gmail.com>
2025-01-19 15:11:28 -0300

    aaa

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TextGeometry } from "three/src/geometries/TextGeometry.js";
@@ -26,10 +25,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 const canvas = renderer.domElement;
 
-const cssrenderer = new CSS3DRenderer();
-cssrenderer.setSize( window.innerWidth, window.innerHeight );
-cssrenderer.domElement.id="renderer"
-container.appendChild( cssrenderer.domElement );
 
 // Background
 
@@ -70,99 +65,17 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
-let size = 0.1 * (canvas.width/1000);
-function drawTextPlane(element) {
-  const plane = new CSS3DObject( element );
-  plane.scale.x = size;
-  plane.scale.y = size;
-  plane.scale.z = size;
-  scene.add( plane );
-  return plane;
-}
 
-function drawAllPlanes() {
-  const sections = document.getElementsByClassName("section");
-  let list = [];
-  for (let i = 0; i < sections.length; i++) {
-    var rect = sections[i].getBoundingClientRect();
-    // const plane = drawTextPlane(
-    //   rect.top / canvas.height,
-    //   rect.bottom / canvas.height,
-    //   rect.left / canvas.width,
-    //   rect.right / canvas.width
-    // );
-    const plane = drawTextPlane(sections[i]);
-    list.push(plane);
-  }
-  return list;
-}
-
-function positionAllPlanes() {
-  size = 0.1 * (canvas.width/1000);
-  const sections = document.getElementsByClassName("section");
-  let list = [];
-  for (let i = 0; i < sections.length; i++) {
-    var rect = sections[i].getBoundingClientRect();
-    planes[i].scale.x = size;
-    planes[i].scale.y = size;
-    planes[i].scale.z = size;
-    // planes[i].position.y = (rect.top / window.innerHeight) * extraSize *-5;
-    // planes[i].position.x = (rect.left / window.innerWidth) * extraSize * 5;
-    // planes[i].position.z = 10;
-  }
-  return list;
-}
-const planes = drawAllPlanes();
-
-// Scroll Animation
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-
-  // camera.position.y = t * 0.01;
-
-  // if(!text) return
-  // let textPosition = new THREE.Vector3();
-  // textPosition = textPosition.copy(text.position).project(camera);
-  // textPosition.x = ((textPosition.x + 1) * canvas.width) / 2;
-  // textPosition.y = (-(textPosition.y - 1) * canvas.height) / 2;
-  // textPosition.x = Math.round(
-  //   (0.5 + textPosition.x / 2) * (canvas.width / window.devicePixelRatio)
-  // );
-  // textPosition.y = Math.round(
-  //   (0.5 - textPosition.y / 2) * (canvas.height / window.devicePixelRatio)
-  // );
-  // console.log(t);
-
-  // const name = document.getElementById("name");
-  // name.innerText = textPosition.y + " - " + textPosition.x;
-  // name.style.top = `${textPosition.y - t}px`;
-  // name.style.left = `${textPosition.x}px`;
-  positionAllPlanes();
-  console.log("pos testing");
-  const sections = document.getElementsByClassName("section");
-  for (let i = 0; i < sections.length; i++) {
-    var rect = sections[i].getBoundingClientRect();
-    // console.log("pos",rect.top,rect.bottom,rect.left,rect.right)
-    // console.log("real",rect.bottom-rect.top,rect.right-rect.left)
-    // console.log("traslate",rect.top/canvas.width ,rect.left/canvas.height )
-    // console.log("scale",(rect.bottom-rect.top)/canvas.width ,(rect.right-rect.left)/canvas.height )
-    console.log("plane", planes[i].position.x, planes[i].position.y);
-  }
-}
 //Rezize
 
 function rezize() {
-  positionAllPlanes()
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  cssrenderer.setSize(window.innerWidth, window.innerHeight);
+  console.log("a")
 }
 
-document.body.onscroll = moveCamera;
 document.body.onresize = rezize;
-moveCamera();
 
 // Animation Loop
 
@@ -170,7 +83,6 @@ function animate() {
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
-  cssrenderer.render(scene, camera);
 }
 
 animate();
