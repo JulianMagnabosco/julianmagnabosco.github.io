96dfbec1ee9310fbd95bd2d1f05eed18e483ae81
Raishin0 <julian.magnabosco@gmail.com>
2025-03-01 17:47:43 -0300

    arreglos menores

--- main.js
+++ main.js
@@ -19,7 +19,9 @@ camera.position.setZ(50);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
+  alpha: true
 });
+renderer.setClearColor( 0x000000, 0 )
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,8 +30,8 @@ const canvas = renderer.domElement;
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
-scene.background = spaceTexture;
+// const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+// scene.background = spaceTexture;
 
 // Lights
 
@@ -42,59 +44,57 @@ scene.background = spaceTexture;
 //Plane
 const segments = 20;
 const geometryPlane = new THREE.PlaneGeometry(24, 24, segments, segments);
-const texturePlane = new THREE.TextureLoader().load('tileset.png' ); 
-texturePlane.wrapS  = THREE.RepeatWrapping;
-texturePlane.wrapT  = THREE.RepeatWrapping;
-texturePlane.repeat.set( segments, segments );
-const materialPlane = new THREE.MeshBasicMaterial( { map:texturePlane } );
+// const texturePlane = new THREE.TextureLoader().load('tileset.png' ); 
+// texturePlane.wrapS  = THREE.RepeatWrapping;
+// texturePlane.wrapT  = THREE.RepeatWrapping;
+// texturePlane.repeat.set( segments, segments );
+const materialPlane = new THREE.MeshBasicMaterial( { color:0x00acff, wireframe:true } );
 geometryPlane.material = materialPlane
 const plane = new THREE.Mesh( geometryPlane, materialPlane );
 
 // const wireframe = new THREE.WireframeGeometry(geometryPlane);
 // const line = new THREE.LineSegments(wireframe, materialPlane);
 
-plane.rotateX(-45);
+plane.rotateX(-1.3);
 plane.position.set(0, 0, 45);
 scene.add(plane);
 
 const heigth = 4;
-const moveDist = 0.001;
+const moveDist = 0.005;
 let dist = 0;
 
 function renderPlane() {
   const vertices = geometryPlane.attributes.position.array;
-  for (let i = 2; i < vertices.length; i += 3) {
-    const x1 = ((i / vertices.length) * (segments + 1) + dist);
+  for (let i = 0; i < vertices.length; i += 3) {
+    const x1 = ((i / vertices.length) * (segments + 1))%1;
     const y1 = i / vertices.length;
-    vertices[i] = perlin.get(x1 * heigth, y1 * heigth);
+    vertices[i+2] = perlin.get(x1 * heigth+dist, y1 * heigth);
   }
-  dist = dist >= 1 ? 0 : dist + moveDist;
+  dist = dist >= 1000 ? 0 : dist + moveDist;
 
   geometryPlane.attributes.position.needsUpdate = true;
   // console.log(dist)
 }
 
 //Stars
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//   const star = new THREE.Mesh(geometry, material);
 
-  let [x, y, z] = [0, 0, 0];
+//   let [x, y, z] = [0, 0, 0];
 
-  x = THREE.MathUtils.randFloatSpread(100);
-  y = THREE.MathUtils.randFloatSpread(200);
-  z = THREE.MathUtils.randFloatSpread(100);
+//   x = THREE.MathUtils.randFloatSpread(100);
+//   y = THREE.MathUtils.randFloatSpread(200);
+//   z = THREE.MathUtils.randFloatSpread(100);
 
-  star.position.set(x, y, z);
-  scene.add(star);
-}
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
 
-Array(200).fill().forEach(addStar);
+// Array(200).fill().forEach(addStar);
 
 //Typing
-let listStrings = [];
-const timing = 2;
 const options = {
   rootMargin: "0px",
   threshold: 0.01,
@@ -107,13 +107,20 @@ const observer = new IntersectionObserver((entries, observer) => {
   });
 }, options);
 
+let listStrings = [];
+const timing = 5;
+const timingScale = 2;
 const listSections = document.getElementsByTagName("section");
 let index = 0;
 for (let i = 0; i < listSections.length; i++) {
   for (let j = 0; j < listSections[i].children.length; j++) {
+    const sectionTiming = timing / listSections[i].children[j].innerHTML.length /timingScale
+    const size = sectionTiming<=0.1?4:1
+    if(listSections[i].children[j].id=="photo") continue
     listStrings.push({
       text: listSections[i].children[j].innerHTML,
-      timing: timing / listSections[i].children[j].innerHTML.length,
+      timing: sectionTiming,
+      size: size,
       index: 0,
     });
 
@@ -146,26 +153,21 @@ function writeText(e) {
 
   const maxLen = listStrings[e.getAttribute("data-id")].text.length-listStrings[e.getAttribute("data-id")].index
 
-  if (
-    oldTextString.length + charEndSize <=
-    listStrings[e.getAttribute("data-id")].index
-  ) {
+  if (oldTextString.length + charEndSize <=listStrings[e.getAttribute("data-id")].index) {
     e.innerHTML = e.innerHTML.replace(regexString, replaceString);
     // console.log(e.getAttribute("data-id")+ " - " +oldTextString.length + " - " +listStrings[e.getAttribute("data-id")].timing);
     return;
   }
 
-  e.innerHTML = oldTextString.substring(
-    0,
-    listStrings[e.getAttribute("data-id")].index
-  );
+  e.innerHTML = oldTextString.substring(0,listStrings[e.getAttribute("data-id")].index);
+
   if (oldTextString.length > listStrings[e.getAttribute("data-id")].index) {
     e.innerHTML +=
       "<span>" +
       charEnds[Math.floor(Math.random() * charEnds.length)].substring(0, maxLen) +
       "</span>";
   }
-  listStrings[e.getAttribute("data-id")].index++;
+  listStrings[e.getAttribute("data-id")].index+=listStrings[e.getAttribute("data-id")].size;
 
   e.innerHTML = e.innerHTML.replace(regexString, replaceString);
 
