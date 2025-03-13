a8f7295bd97c117cfee0e78e93a81ca5b568bd05
Raishin0 <julian.magnabosco@gmail.com>
2025-03-13 01:42:07 -0300

    casi

---
+++ smartnav.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+console.log(window.innerWidth)
+let toggle = false
+const element = document.getElementsByClassName("nav-bar")[0]
+const button = document.getElementsByClassName("nav-bar-button")[0]
+const otherbuttons = Array.from(element.getElementsByTagName("a"))
+console.log(otherbuttons)
+
+function toggleOpen() {
+  element.classList.add('open');
+  if (toggle) {
+    element.classList.remove('open');
+  }
+  toggle=!toggle
+}
+
+button.addEventListener('click', toggleOpen);
+otherbuttons.forEach((ob) => ob.addEventListener('click', toggleOpen));
